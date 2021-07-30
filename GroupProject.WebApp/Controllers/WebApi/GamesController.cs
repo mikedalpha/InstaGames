1703869772cbc534cc288f04d9a8d97a202454f7
Kostas <Kwstaskor@hotmail.com>
2021-07-30 16:02:40 +0300

    Details View

--- GroupProject.WebApp/Controllers/WebApi/GamesController.cs
+++ GroupProject.WebApp/Controllers/WebApi/GamesController.cs
@@ -7,38 +7,69 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using GroupProject.Database;
 using GroupProject.Entities.Domain_Models;
+using GroupProject.RepositoryService;
 
 namespace GroupProject.WebApp.Controllers.WebApi
 {
     public class GamesController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly IUnitOfWork unitOfWork;
+
+        public GamesController()
+        {
+            unitOfWork = new UnitOfWork();
+        }
 
         // GET: api/Games
         public async Task<IHttpActionResult> GetGames()
         {
-            return Ok(await db.Games.Select(g=>new
+            var  games = await unitOfWork.Games.GetAllAsync();
+
+            return Ok( games.Select(g=>new
             {
                 GameId = g.GameId,
                 Title = g.Title,
                 Photo=g.Photo,
                 Descripition =g.Description,
                 Pegi = g.Pegi,
-                ReleaseDate =g.ReleaseDate.Year
-            }).ToListAsync());
+                ReleaseDate =g.ReleaseDate.Year,
+                IsRealeased = g.IsReleased,
+                Rating = g.Rating,
+                Tag = g.Tag.ToString()
+            }).ToList());
         }
 
         // GET: api/Games/5
         [ResponseType(typeof(Game))]
         public async Task<IHttpActionResult> GetGame(int id)
         {
-            Game game = await db.Games.FindAsync(id);
+            var game = await unitOfWork.Games.FindByIdAsync(id);
             if (game == null)
             {
                 return NotFound();
             }
 
-            return Ok(game);
+            return Ok(new
+            {
+                GameId=game.GameId,
+                Title=game.Title,
+                Photo = game.Photo,
+                Descripition = game.Description,
+                Pegi = game.Pegi,
+                ReleaseDate = game.ReleaseDate.Year,
+                Rating = game.Rating,
+                IsRealeased=game.IsReleased,
+                Tag=game.Tag.ToString(),
+                Categories = new
+                {
+                    Type = game.GameCategories.Select(c=>c.Type)
+                },
+                Developer = new
+                {
+                    Name =game.GameDevelopers.Select(d => d.FirstName +" " + d.LastName)
+                }
+            });
         }
 
         // PUT: api/Games/5
