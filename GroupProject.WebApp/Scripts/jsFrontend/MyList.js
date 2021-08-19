dea830abe7743323e913ec53857bccf77e91cc88
Konstantina <konstantinalakoumenta@gmail.com>
2021-08-19 19:29:09 +0300

    my list, new layout

--- GroupProject.WebApp/Scripts/jsFrontend/MyList.js
+++ GroupProject.WebApp/Scripts/jsFrontend/MyList.js
@@ -1,4 +1,3 @@
-﻿
 
 jQuery('.addToList').click(function (e) {
     let icon = $(this);
@@ -6,20 +5,24 @@ jQuery('.addToList').click(function (e) {
     AddToList(userId, $(this).data('id'), icon);
 });
 
-
 function AddToList(id, gameId, icon) {
+  
     let url = `https://localhost:44369/api/Account/?id=${id}&gameid=${gameId}`;
     $.ajax({
         type: "PUT",
         url: url,
         dataType: "json",
         success: function (response) {
+
             if (icon.find('i').hasClass('ri-add-line')) {
+
                 icon.find('i').removeClass('ri-add-line');
                 icon.find('i').addClass('fa fa-check-circle-o');
+              
             } else {
                 icon.find('i').removeClass('fa fa-check-circle-o');
                 icon.find('i').addClass('ri-add-line');
+               
             }
         }
     });
