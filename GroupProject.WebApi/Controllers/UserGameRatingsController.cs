d5110c88a1615a3f5a0247818e4370b4c652e734
Kostas <Kwstaskor@hotmail.com>
2021-08-22 09:23:59 +0300

    Auto stash before rebase of "master"
    Tipota

--- GroupProject.WebApi/Controllers/UserGameRatingsController.cs
+++ GroupProject.WebApi/Controllers/UserGameRatingsController.cs
@@ -2,7 +2,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Cors;
 using GroupProject.Entities.Domain_Models;
 using GroupProject.RepositoryService;
 using Microsoft.AspNet.Identity.Owin;
